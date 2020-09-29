67e7b82269a8778758071bf2c0389dc74f7d1349
Brenden2008 <42918416+Brenden2008@users.noreply.github.com>
2020-09-28 20:08:48 -0500

    Create keyinjector.js

---
+++ injectors/keyinjector.js
@@ -0,0 +1,6 @@
+javascript:(function(){
+	var script=document.createElement('script');
+	script.type='text/javascript';
+	script.src='https://cdn.jsdelivr.net/gh/Brenden2008/InvisableKeyCounter@master/source/mainkey.js';
+	document.getElementsByTagName('head')[0].appendChild(script);
+})();
