38c58eafb965e0345cdb3f32c4e6070bb5f63523
Brenden2008 <42918416+Brenden2008@users.noreply.github.com>
2020-09-28 20:07:22 -0500

    Update spaceinjector.js

--- injectors/spaceinjector.js
+++ injectors/spaceinjector.js
@@ -1,6 +1,6 @@
 javascript:(function(){
 	var script=document.createElement('script');
 	script.type='text/javascript';
-	script.src='https://gitcdn.xyz/repo/Brenden2008/InvisableKeyCounter/master/source/mainspacebar.js';
+	script.src='https://cdn.jsdelivr.net/gh/Brenden2008/InvisableKeyCounter@master/source/mainspacebar.js';
 	document.getElementsByTagName('head')[0].appendChild(script);
 })();
