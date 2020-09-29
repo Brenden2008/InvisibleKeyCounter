16295db9b6293aad5edf9ffc7380e7b92ec0b8c7
Brenden2008 <42918416+Brenden2008@users.noreply.github.com>
2020-09-28 20:08:09 -0500

    Create guiinjector.js

---
+++ injectors/guiinjector.js
@@ -0,0 +1,6 @@
+javascript:(function(){
+	var script=document.createElement('script');
+	script.type='text/javascript';
+	script.src='https://cdn.jsdelivr.net/gh/Brenden2008/InvisableKeyCounter@master/source/maingui.js';
+	document.getElementsByTagName('head')[0].appendChild(script);
+})();
